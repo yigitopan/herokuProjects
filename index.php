89435a62b1edaa90d7e06764ab91316eba483c58
yigitopan <yigitopan@hotmail.com>
2021-08-09 14:02:29 +0300

    json local denemesi

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$json = file_get_contents('http://localhost:3000/posts');
+$json = file_get_contents('https://jsonplaceholder.typicode.com/posts');
 $obj = json_decode($json, true);
 foreach ($obj as $key => $value){
-    $out=$value["id"];
+    $out=$value["id.,i"];
     echo $out ."<br/>";
 }
 echo "deneme";
