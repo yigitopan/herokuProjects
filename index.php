fb0ed69dff81d97db2836a3334f04dab77277d1d
yigitopan <yigitopan@hotmail.com>
2021-08-09 18:49:53 +0300

    d

--- index.php
+++ index.php
@@ -1,39 +1,30 @@
 <?php
 
-$url = 'https://jsonplaceholder.typicode.com/posts';
+$url = "https://reqres.in/api/users";
 
 $data_array = array(
-    'userId' => 102,
-    'id' => 123456,
-    'title' => '1235456',
-    'body' => '1233456'
+    'name' => 'John Doe',
+    'job' => 'Web Developer'
 );
-$data1 = http_build_query($data_array);
+
+$data = http_build_query($data_array);
 
 $ch = curl_init();
 
-
-
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, $url);
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST,true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $resp = curl_exec($ch);
-if($e = curl_error($ch))
-{
-    echo $e."</br>";
+
+if($e = curl_error($ch)){
+    echo "hata: ".$e;
 }
-
-
-
-$data = file_get_contents('https://jsonplaceholder.typicode.com/posts');
-$json_arr = json_decode($resp);
-$titleId = 0;
-foreach ($json_arr as $key => $value){
-    $out=$value["title"];
-    echo "title $titleId :".$out ."<br/>";
-    $titleId++;
+else {
+    $decoded = json_decode($resp);
+    foreach ($decoded as $key => $val){
+        echo $key . ': '. $val . '<br>';
+    }
 }
-echo "deneme";
 ?>
