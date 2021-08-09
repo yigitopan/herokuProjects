3baf8083c5a7014dbf682dbba1b9f18d68e22d65
yigitopan <yigitopan@hotmail.com>
2021-08-09 13:24:23 +0300

    json local denemesi

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 $json = file_get_contents('http://localhost:3000/');
-$obj = json_decode($json);
-echo $obj->posts;
+$obj = json_decode($json, true);
+foreach ($obj as $key => $value){
+    $out=$value["email"];
+    echo $out ."<br/>";
+}
 echo "deneme";
 ?>
