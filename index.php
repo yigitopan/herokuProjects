bd39c859c061355b660f311bed258113f3c767c9
yigitopan <yigitopan@hotmail.com>
2021-08-09 15:41:30 +0300

    d

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $json = file_get_contents('https://jsonplaceholder.typicode.com/posts');
 $obj = json_decode($json, true);
 $titleId = 0;
+unset($obj[35]);
 foreach ($obj as $key => $value){
     $out=$value["title"];
     echo "title $titleId :".$out ."<br/>";
