92fe90f65a582658a709c6c74ddca963821ba289
yigitopan <yigitopan@hotmail.com>
2021-08-09 16:45:51 +0300

    d

--- index.php
+++ index.php
@@ -1,38 +1,35 @@
 <?php
+
 $url = 'https://jsonplaceholder.typicode.com/posts';
 
-// Create a new cURL resource
-$ch = curl_init($url);
-
-// Setup request to send json via POST
-$data2 = array(
-    'userId' => 'codexworld',
+$data_array = array(
+    'userId' => 102,
     'id' => 123456,
     'title' => '123456',
     'body' => '1233456'
 );
-$payload = json_encode(array("post" => $data2));
+$data1 = http_build_query($data_array);
 
-// Attach encoded JSON string to the POST fields
-curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+$ch = curl_init();
 
-// Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 
-// Return response instead of outputting
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// Execute the POST request
-$result = curl_exec($ch);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, $url);
 
-// Close cURL resource
-curl_close($ch);
+$resp = curl_exec($ch);
+if($e = curl_error($ch))
+{
+    echo $e."</br>";
+}
+
 
 
 $data = file_get_contents('https://jsonplaceholder.typicode.com/posts');
 $json_arr = json_decode($data, true);
 $titleId = 0;
-unset($json_arr[35]);
 foreach ($json_arr as $key => $value){
     $out=$value["title"];
     echo "title $titleId :".$out ."<br/>";
