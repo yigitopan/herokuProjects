f49979f53e6ebab0ccafd8d4264558a2066b938e
yigitopan <yigitopan@hotmail.com>
2021-08-09 14:07:56 +0300

    son deneme

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 $json = file_get_contents('https://jsonplaceholder.typicode.com/posts');
 $obj = json_decode($json, true);
+$titleId = 0;
 foreach ($obj as $key => $value){
-    $out=$value["id.,i"];
-    echo $out ."<br/>";
+    $out=$value["title"];
+    echo "title $titleId :".$out ."<br/>";
+    $titleId++;
 }
 echo "deneme";
 ?>
