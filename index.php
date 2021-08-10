f16982aec3ebe580d35ddd7a95ee121d9766fcdb
yigitopan <yigitopan@hotmail.com>
2021-08-10 13:34:16 +0300

    put deneme

--- index.php
+++ index.php
@@ -33,9 +33,8 @@ else {
 }
 
                POST BITISSS*/
-
-/* GET START
-GET BITIS */
+//------------------------------------------------------------------------------------------------------------------
+                    /* GET START
 
 $ch = curl_init();
 
@@ -53,5 +52,37 @@ else {
     $decoded = json_decode($resp);
     print_r($decoded);
 }
+                        GET BITIS */
+
+$url = "https://jsonplaceholder.typicode.com/posts/1";
+
+$data_array = array(
+    'userId' => 201,
+    'id' => 999,
+    'title' => 'Web Devel42oper',
+    'body' => 'Web Devel4oper',
+);
+
+$data = http_build_query($data_array);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'PUT');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$resp = curl_exec($ch);
+
+if($e = curl_error($ch)){
+    echo "hata: ".$e;
+}
+else {
+    $decoded = json_decode($resp);
+    foreach ($decoded as $key => $val){
+        echo $key . ': '. $val . '<br>';
+    }
+}
+
 
 ?>
